7db4a2fb114c29ecdbcec5f03827d8804d788d8c
Rodrigo <Rodrigo.neves@fatec.sp.gov>
2024-03-10 17:29:33 -0300

    Merge Sort feito

--- Bubble e Merge Sort/src/controller/Merge_Sort.java
+++ Bubble e Merge Sort/src/controller/Merge_Sort.java
@@ -5,7 +5,44 @@ public class Merge_Sort {
         super();
     }
 
-    public void mergeSort(int[] vetor, int tamanho){
+    public void mergeSort(int[] vetor, int inicio, int fim){
+        if (inicio < fim){
+            int meio = (inicio+fim)/2;
+            mergeSort(vetor, inicio,meio);
+            mergeSort(vetor,meio+1, fim);
+            intercala(vetor, inicio,meio,fim);
+        }
+    }
+
+    private void intercala(int[] vetor, int inicio, int meio, int fim) {
+        int[] newVetor  = new int[vetor.length];
+        int esquerda = inicio;
+        int direita = meio +1;
+
+
+        for(int cont = inicio; cont <= fim; cont++){
+            newVetor[cont] = vetor[cont];
+        }
+
+        for (int cont=inicio; cont <= fim;cont++){
+            
+            if (esquerda>meio){
+                vetor[cont] = newVetor[direita];
+                direita++;
+
+            }else if (direita > fim){
+                vetor[cont] = newVetor[esquerda];
+                esquerda++;
+                
+            } else if (newVetor[esquerda] < newVetor[direita]) {
+                vetor[cont] = newVetor[esquerda];
+                esquerda++;
+                
+            }else {
+                vetor[cont] = newVetor[direita];
+                direita++;
+            }
+        }
 
     }
 }
